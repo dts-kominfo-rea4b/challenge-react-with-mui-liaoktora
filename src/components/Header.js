d19936de71463c4b7f7ea181c3150571f53989ca
liaoktora <113673764+liaoktora@users.noreply.github.com>
2022-09-25 20:24:40 +0700

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,21 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
 // atau langsung tambahkan dengan sx={{}}
 import React from 'react';
+import { Grid, Typography } from '@mui/material';
 
 // Header berisi judul dan subtitle
 // Judul: Call a Friend
 // Subtitle: Your friendly contact app
 const Header = () => {
   return (
-    <></>
+    <Grid item xs={12}>
+      <Typography variant="h1" component="h1" gutterBottom>
+        Call a Friend
+      </Typography>
+      <Typography variant="h6" component="h2" gutterBottom>
+        Your friendly contact app
+      </Typography>
+    </Grid>  
   );
 };
 
